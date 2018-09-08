ce3caf2a46bfd0a13ee04a83426d21525f99507c
Frank Lefebvre <dev@franklefebvre.com>
2018-09-08 15:53:11 +0200

    Update unit tests.

--- Tests/XMLCoderTests/XMLCoderTests.swift
+++ Tests/XMLCoderTests/XMLCoderTests.swift
@@ -2,7 +2,7 @@ import XCTest
 @testable import XMLCoder
 
 final class XMLCoderTests: XCTestCase {
-    func testExample() {
+    func testEncodeBasicXML() {
 		struct TestStruct: Encodable {
 			var integer_element: Int
 			var string_element: String
@@ -16,21 +16,39 @@ final class XMLCoderTests: XCTestCase {
 		}
 
 		let embedded = EmbeddedStruct(some_element: "inside")
-		let value = TestStruct(integer_element: 42, string_element: "moof!", embedded_element: embedded, string_array: ["one", "two", "three"], int_array: [1, 2, 3])
+		let value = TestStruct(integer_element: 42, string_element: "   moof   & < >   ", embedded_element: embedded, string_array: ["one", "two", "three"], int_array: [1, 2, 3])
 
 		let encoder = XMLEncoder()
 		let xml = try! encoder.encode(value)
 		let result = String(data: xml.xmlData, encoding: .utf8)
 		
         let expected = """
-        <root><integer_element>42</integer_element><string_element>moof!</string_element><embedded_element><some_element>inside</some_element></embedded_element><string_array><element>one</element><element>two</element><element>three</element></string_array><int_array><element>1</element><element>2</element><element>3</element></int_array></root>
+        <root>\
+        <integer_element>42</integer_element>\
+        <string_element>   moof   &amp; &lt; &gt;   </string_element>\
+        <embedded_element><some_element>inside</some_element></embedded_element>\
+        <string_array><element>one</element><element>two</element><element>three</element></string_array>\
+        <int_array><element>1</element><element>2</element><element>3</element></int_array>\
+        </root>
         """
         
         XCTAssertEqual(result, expected)
     }
+    
+    func testWhitespace() throws {
+        let initialString = """
+        <?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\
+        <whitespace xml:space="collapse">   a   b   c
+        d   e   f   </whitespace>
+        """
+        let xml = try XMLDocument(xmlString: initialString)
+        let finalString = String(data: xml.xmlData, encoding: .utf8)
+        XCTAssertEqual(initialString, finalString)
+    }
 
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testEncodeBasicXML", testEncodeBasicXML),
+        ("testWhitespace", testWhitespace),
     ]
 }
