2ef8e0b2e33120edaad289e1bc2d2108e3cf293b
Frank Lefebvre <dev@franklefebvre.com>
2020-05-02 13:09:16 +0200

    Add Combine support.

---
+++ Sources/XMLCoder/CombineSupport.swift
@@ -0,0 +1,16 @@
+//
+//  CombineSupport.swift
+//  
+//
+//  Created by frank on 02/05/2020.
+//
+
+#if canImport(Combine)
+
+import Combine
+
+extension XMLDecoder: TopLevelDecoder {}
+extension XMLEncoder: TopLevelEncoder {}
+
+#endif
+
